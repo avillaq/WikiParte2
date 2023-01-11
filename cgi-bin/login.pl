fa6a0646c8c74fe504949e95fefb8765d9902454
avillaq <villafuertequispealex@gmail.com>
2023-01-11 01:08:57 -0500

    Mejorando algunas cosas

--- cgi-bin/login.pl
+++ cgi-bin/login.pl
@@ -2,30 +2,56 @@
 use strict;
 use CGI;
 use DBI;
-
 my $q = CGI->new;
 my $name = $q->param('name');
 my $password = $q->param('password');
-
+print "Content-Type: text/xml;\n";
+print<<XML;
+<?xml version='1.0' encoding='utf-8'?>
+    <user>
+        <owner></owner>
+        <firstName></firstName>
+        <lastName></lastName>
+    </user>
+XML
+=pot
 if($name eq "" || $password eq ""){
-    print "Location: ../login.html \n\n";
+    
 }
 else{
-    my $dsn = "DBI:mysql:database=datostienda;host=127.0.0.1";
+    my $dsn = "DBI:mysql:database=datospaginaxml;host=127.0.0.1";
     my $dbh = DBI->connect($dsn, "Alex", "") or die "No se pudo conectar";
     
-    my $sth = $dbh->prepare("SELECT userName, password FROM users WHERE name=? AND password=?");
+    my $sth = $dbh->prepare("SELECT userName, password FROM users WHERE userName=? AND password=?");
     $sth->execute($name, $password);
     
     my @array = $sth->fetchrow_array;
 
     $dbh->disconnect;
 
-    #Si el array esta vacio significa que no existe el usuario
-    if(@array != 0){
-        print "Location: ./list.pl \n\n"; 
-    }
-    else{
-        print "Location: ../index.html \n\n";
-    }
+    
+
 }
+sub datosCorrectos{
+    print $q->header('text/xml');    
+    print<<XML;
+    <?xml version='1.0' encoding='utf-8'?>
+        <user>
+            <owner></owner>
+            <firstName></firstName>
+            <lastName></lastName>
+        </user>
+XML
+}
+sub datosIncorrectos{
+    print $q->header('text/xml');    
+    print<<XML;
+    <?xml version='1.0' encoding='utf-8'?>
+        <user>
+            <owner></owner>
+            <firstName></firstName>
+            <lastName></lastName>
+        </user>
+XML 
+}
+=cut
