2bf50dee9537166f5bbca83a3cca4d33fd8a3204
avillaq <villafuertequispealex@gmail.com>
2023-01-12 01:00:05 -0500

    Se eliminaron varios archivos html con formularios y se los puso todos en entrada.pl

--- cgi-bin/entrada.pl
+++ cgi-bin/entrada.pl
@@ -6,27 +6,112 @@ my $q = CGI->new;
 
 my $entrada = $q->param('entrada');
 
+print $q->header('text/html');    
+print<<HTML;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../styles/general.css">
+    <link rel="stylesheet" href="../styles/formGeneral.css">
+    <title>Wiki</title>
+</head>
+<body>
+    <div class="wrap">
+HTML
+my $form="";
+
 if ($entrada eq "login") {
-    print "Location: ../login.html \n\n";
+    $form="<form action='./login.pl' method='post'>
+                <label>Usuario</label>
+                <input class='inputText' type='text' name='name' required>
+
+                <label>Contrasena</label>
+                <input class='inputText' type='password' name='password' required>
+                <br>";
+                
+
 }
 elsif ($entrada eq "register") {
-    print "Location: ../register.html \n\n";
+    $form = "<form action='./register.pl' method='post' style='height: 350px;'>
+                <label>Nombre</label>
+                <input type='text' class='inputText' name='newfirstname' required>
+                
+                <label>Apellido</label>
+                <input type='text' class='inputText' name='newlastname' required>
+                
+                <label>Nombre de usuario</label>
+                <input type='text' class='inputText' name='newname' required>
+                
+                <label>Contrasena</label>
+                <input type='password' class='inputText' name='newpassword' required>
+                <br>";
 }
 elsif ($entrada eq "new") {
-    print "Location: ../new.html \n\n";
+    $form = "<form action='./new.pl' method='post'>
+            <label>Titulo</label>
+            <input class='inputText' type='text' name='titulo'>
+
+            <label>Texto</label>
+            <textarea class='inputTextArea' name='texto' rows='8'></textarea>
+
+            <label>Nombre de usuario</label>
+            <input class='inputText' type='text' name='usuario'><br>
+
+            <input type='hidden' name='esNuevo' value='true'>";
 }
 elsif ($entrada eq "list") {
-    print "Location: ../list.html \n\n";
+    $form = "<form action='./list.pl' method='post'>
+            <label>Nombre de usuario</label>
+            <input class='inputText' type='text' name='usuario'>
+            <br>";
 }
 elsif ($entrada eq "view") {
-    print "Location: ../view.html \n\n";
+    $form = "<form action='./view.pl' method='post'>
+            <label>Nombre de usuario</label>
+            <input class='inputText' type='text' name='usuario'>
+
+            <label>Titulo</label>
+            <input class='inputText' type='text' name='titulo'>
+            <br>";
 }
 elsif ($entrada eq "delete") {
-    print "Location: ../delete.html \n\n";
+    $form = "<form action='./delete.pl' method='post'>
+            <label>Nombre de usuario</label>
+            <input class='inputText' type='text' name='usuario'>
+
+            <label>Titulo</label>
+            <input class='inputText' type='text' name='titulo'>
+            <br>";
 }
 elsif ($entrada eq "article") {
-    print "Location: ../article.html \n\n";
+    $form = "<form action='./article.pl' method='post'>
+            <label>Nombre de usuario</label>
+            <input class='inputText' type='text' name='usuario'>
+
+            <label>Titulo</label>
+            <input class='inputText' type='text' name='titulo'>
+            <br>";
 }
 elsif ($entrada eq "update") {
-    print "Location: ../update.html \n\n";
+    $form = "<form action='./new.pl' method='post'>
+            <label>Titulo</label>
+            <input class='inputText' type='text' name='titulo'>
+
+            <label>Texto</label>
+            <textarea class='inputTextArea' name='texto' rows='8'></textarea>
+
+            <label>Nombre de usuario</label>
+            <input class='inputText' type='text' name='usuario'><br>
+
+            <input type='hidden' name='esNuevo' value='false'>";
 }
+
+print<<HTML;
+            $form
+            <input class="inputSubmit" type="submit" value="Enviar">
+        </form>
+    </div>
+</body>
+</html>
+HTML
